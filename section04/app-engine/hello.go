bcfc9fdd5089eb214091a552256276b7637c0fbe
Charlie.Chen <charlie75512@gmail.com>
2018-06-15 00:30:52 +0800

    section04 homework

---
+++ section04/app-engine/hello.go
@@ -0,0 +1,33 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, App Engine")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// first create a new context
+	c := appengine.NewContext(r)
+	// and use that context to create a new http client
+	client := urlfetch.Client(c)
+
+	// now we can use that http client as before
+	res, err := client.Get("http://google.com")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("could not get google: %v", err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Got Google with status %s\n", res.Status)
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/hello", helloHandler)
+}
